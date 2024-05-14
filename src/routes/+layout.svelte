c5d53a686ce493c0318e37df6f7f17441a6b3fe0
Adam Henderson <adamhenderson90@gmail.com>
2024-05-14 21:40:01 +0100

    Add seo content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { page } from "$app/stores";
     import Footer from "$lib/blocks/Footer.svelte";
     import LoginModal from "$lib/blocks/LoginModal.svelte";
     import Navigation from "$lib/blocks/Navigation.svelte";
@@ -26,6 +27,16 @@
         loginModal: { ref: LoginModal },
     };
 </script>
+
+<svelte:head>
+    <title>{$page.data.seo.title}</title>
+    <meta name="description" content={$page.data.seo.description} />
+    <link
+        rel="canonical"
+        href={`https://www.example.com/${$page.data.seo.canonicalPage}`}
+    />
+    <meta name="robots" content="index, follow" />
+</svelte:head>
 
 <Modal components={modalRegistry} />
 
---
+++ src/routes/+page.ts
@@ -0,0 +1,13 @@
+/* Required for every page  */
+
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ data }) => {
+    return {
+        seo: {
+            title: "",
+            description: "",
+            canonicalPage: "",
+        }
+    };
+};
