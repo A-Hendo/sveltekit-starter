c7c5ca4214ce012c5f1c5e0c532b7be0fef562ef
Adam Henderson <adamhenderson90@gmail.com>
2024-05-17 13:57:19 +0100

    Update header name to Hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,11 @@
     import Faq from "$lib/blocks/FAQ.svelte";
     import Features from "$lib/blocks/Features.svelte";
     import FeaturesImage from "$lib/blocks/FeaturesImage.svelte";
-    import Header from "$lib/blocks/Header.svelte";
+    import Hero from "$lib/blocks/Hero.svelte";
     import Pricing from "$lib/blocks/Pricing.svelte";
 </script>
 
-<Header />
+<Hero />
 <Features />
 <FeaturesImage />
 <Pricing />
