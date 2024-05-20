dc8cf4ce82aaf728d857cc925d94c3cdf6c385af
Adam Henderson <adamhenderson90@gmail.com>
2024-05-20 13:57:51 +0100

    Add a horizontial hero component

---
+++ src/lib/blocks/HeroHorizontial.svelte
@@ -0,0 +1,26 @@
+<div class="">
+    <div class="mx-auto max-w-screen-xl px-3 sm:px-5 lg:px-6 py-24">
+        <header class="flex text-center gap-12 md:flex-nowrap flex-wrap">
+            <div
+                class="gap-10 items-center justify-center text-center lg:w-full md:text-left md:items-start"
+            >
+                <h1 class="h2 font-bold whitespace-pre-line">
+                    Lorem ipsum dolor sit amet
+                    <span class="text-primary-500">Consectetur</span>
+                </h1>
+                <p class="my-6 whitespace-pre-line text-2xl">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                </p>
+                <a href="/" class="btn variant-filled-primary">Get Started</a>
+            </div>
+            <div class="flex">
+                <img
+                    src="https://placehold.co/600x400"
+                    alt="product screenshot"
+                />
+            </div>
+        </header>
+    </div>
+</div>
