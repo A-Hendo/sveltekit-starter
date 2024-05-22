6daa6d9845ca3b20039dff84fa4508ad3973337f
Adam Henderson <adamhenderson90@gmail.com>
2024-05-22 12:24:23 +0100

    Add new footer component with links and logo

---
+++ src/lib/blocks/FooterLogo.svelte
@@ -0,0 +1,32 @@
+<footer
+    class="p-10 grid md:grid-flow-col grid-flow-row gap-y-6 justify-center md:justify-normal"
+>
+    <aside class="grid justify-center">
+        <img
+            src="https://placehold.co/50x50"
+            alt="company logo"
+            class="justify-self-center"
+        />
+        <p>your company Ltd</p>
+    </aside>
+    <nav class="grid grid-flow-row text-center sm:text-left">
+        <h6 class="footer-title">Header</h6>
+        <a href="/" class="btn-link">New link</a>
+        <a href="/" class="btn-link">New link</a>
+        <a href="/" class="btn-link">New link</a>
+        <a href="/" class="btn-link">New link</a>
+    </nav>
+    <nav class="grid grid-flow-row text-center sm:text-left">
+        <h6 class="footer-title">Header</h6>
+        <a href="/" class="btn-link">New link</a>
+        <a href="/" class="btn-link">New link</a>
+        <a href="/" class="btn-link">New link</a>
+        <a href="/" class="btn-link">New link</a>
+    </nav>
+    <nav class="grid grid-flow-row text-center sm:text-left">
+        <h6 class="footer-title">Header</h6>
+        <a href="/" class="btn-link">New link</a>
+        <a href="/" class="btn-link">New link</a>
+        <a href="/" class="btn-link">New link</a>
+    </nav>
+</footer>
